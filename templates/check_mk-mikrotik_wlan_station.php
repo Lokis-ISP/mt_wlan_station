2b43e860b575c4ada169ca3dc5931bb9498899c3
Łukasz Górski <moon.musick@gmail.com>
2015-04-22 20:45:03 +0200

    Fix PNP4Nagios template
    Modified template to use PNP4Nagios helper functions only.

--- templates/check_mk-mikrotik_wlan_station.php
+++ templates/check_mk-mikrotik_wlan_station.php
@@ -1,36 +1,38 @@
 <?php
-$opt[1] = "--vertical-label Mbps --title \"TX/RX for $hostname / $servicedesc\" ";
+$opt[1] =  "--vertical-label Mbps --title \"TX/RX for $hostname / $servicedesc\" ";
 
-$def[1] =   "DEF:tx=$RRDFILE[1]:$DS[1]:AVERAGE ";
-$def[1] .=  "HRULE:$WARN[1]#FFFF00 ";
-$def[1] .=  "HRULE:$CRIT[1]#FF0000 ";
-$def[1] .=  "HRULE:-$WARN[2]#FFFF00 ";
-$def[1] .=  "HRULE:-$CRIT[2]#FF0000 ";
-$def[1] .=  "AREA:tx#76D5D6:\"TX\l\" ";
-$def[1] .=  "DEF:rx=$RRDFILE[1]:$DS[2]:AVERAGE ";
-$def[1] .=  "CDEF:rx_neg=rx,-1,* ";
-$def[1] .=  "AREA:rx_neg#7BED8C:\"RX\l\" ";
-$def[1] .=  "COMMENT:\"\u\" ";
-$def[1] .=  "COMMENT:\"\u\" ";
-$def[1] .=  "GPRINT:tx:LAST:\"        Last %2.0lf Mbit\" ";
-$def[1] .=  "GPRINT:tx:MAX:\"Max %2.0lf Mbit\" ";
-$def[1] .=  "GPRINT:tx:MIN:\"Min %2.0lf Mbit\" ";
-$def[1] .=  "GPRINT:tx:AVERAGE:\"Average %2.0lf Mbit\l\" ";
-$def[1] .=  "GPRINT:rx:LAST:\"        Last %2.0lf Mbit\" ";
-$def[1] .=  "GPRINT:rx:MAX:\"Max %2.0lf Mbit\" ";
-$def[1] .=  "GPRINT:rx:MIN:\"Min %2.0lf Mbit\" ";
-$def[1] .=  "GPRINT:rx:AVERAGE:\"Average %2.0lf Mbit\l\" ";
+$def[1] =   rrd::def('tx', $RRDFILE[1], $DS[1], 'AVERAGE');
+$def[1] .=  rrd::hrule($WARN[1], '#ffff00');
+$def[1] .=  rrd::hrule($CRIT[1], '#ff0000');
+$def[1] .=  rrd::hrule(-$WARN[2], '#ffff00');
+$def[1] .=  rrd::hrule(-$CRIT[2], '#ff0000');
+$def[1] .=  rrd::area('tx', '#76D5D6', 'TX\l');
+$def[1] .=  rrd::def('rx', $RRDFILE[1], $DS[2], 'AVERAGE');
+$def[1] .=  rrd::cdef('rx_neg', 'rx,-1,*');
+$def[1] .=  rrd::area('rx_neg', '#7BED8C', 'RX\l');
+// move the next lines up
+$def[1] .=  rrd::comment('\u');
+$def[1] .=  rrd::comment('\u');
+$def[1] .=  rrd::gprint('tx', 'LAST',    '        Last %3.0lf Mbit');
+$def[1] .=  rrd::gprint('tx', 'MAX',     '\tMax %3.0lf Mbit');
+$def[1] .=  rrd::gprint('tx', 'MIN',     '\tMin %3.0lf Mbit');
+$def[1] .=  rrd::gprint('tx', 'AVERAGE', '\tAverage %3.0lf Mbit\l');
+$def[1] .=  rrd::gprint('rx', 'LAST',    '        Last %3.0lf Mbit');
+$def[1] .=  rrd::gprint('rx', 'MAX',     '\tMax %3.0lf Mbit');
+$def[1] .=  rrd::gprint('rx', 'MIN',     '\tMin %3.0lf Mbit');
+$def[1] .=  rrd::gprint('rx', 'AVERAGE', '\tAverage %3.0lf Mbit\l');
 
 
-$opt[2] = "--vertical-label dB --title \"Signal strength for $hostname / $servicedesc\" ";
+$opt[2] =  "--vertical-label dB --title \"Signal strength for $hostname / $servicedesc\" ";
 
-$def[2] =   "DEF:signal=$RRDFILE[1]:$DS[3]:AVERAGE ";
-$def[2] .=  "HRULE:$WARN[3]#FFFF00 ";
-$def[2] .=  "HRULE:$CRIT[3]#FF0000 ";
-$def[2] .=  "AREA:signal#888888:\"Signal\l\" ";
-$def[2] .=  "COMMENT:\"\u\" ";
-$def[2] .=  "GPRINT:signal:LAST:\"          Last %2.0lf dB\" ";
-$def[2] .=  "GPRINT:signal:MAX:\"Max %2.0lf dB\" ";
-$def[2] .=  "GPRINT:signal:MIN:\"Min %2.0lf dB\" ";
-$def[2] .=  "GPRINT:signal:AVERAGE:\"Average %2.0lf dB\l\" ";
+$def[2] =   rrd::def('signal', $RRDFILE[1], $DS[3], 'AVERAGE');
+$def[2] .=  rrd::hrule($WARN[3], '#ffff00');
+$def[2] .=  rrd::hrule($CRIT[3], '#ff0000');
+$def[2] .=  rrd::area('signal', '#888888', 'Signal\l');
+// move the next lines up
+$def[2] .=  rrd::comment('\u');
+$def[2] .=  rrd::gprint('signal', 'LAST',    '          Last %2.0lf dB');
+$def[2] .=  rrd::gprint('signal', 'MAX',     '\tMax %2.0lf dB');
+$def[2] .=  rrd::gprint('signal', 'MIN',     '\tMin %2.0lf dB');
+$def[2] .=  rrd::gprint('signal', 'AVERAGE', '\tAverage %2.0lf dB\l');
 ?>
